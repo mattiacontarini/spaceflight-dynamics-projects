30839d3afa29b9f940a2eed9e081715786329aff
mcontarini <mcontarini@student.tudelft.nl>
2026-04-23 11:43:58 +0200

    Fix branch

---
+++ TransferProblem.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import os
+
+
+
+class TransferProblem:
+
+    def __init__(
+            self,
+            bodies,
+            ):
+        ...
+
+    def propagate_trajectory(
+            self,
+            initial_state
+            ):
+        ...
