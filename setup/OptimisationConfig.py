aba7fa158412991edab8fe9a2e30b32e681ed819
mcontarini <mcontarini@student.tudelft.nl>
2026-05-02 17:02:22 +0200

    Set Earth parking orbit parameters

---
+++ setup/OptimisationConfig.py
@@ -0,0 +1,7 @@
+# Earth parking orbit parameters
+earth_parking_orbit_ecc = 0.02  # [-]
+earth_parking_orbit_alt = 400.0e3  # [m]
+earth_parking_orbit_inclination = 0.0  # [rad]
+earth_parking_orbit_true_anomaly = 0.0  # [rad]
+
+
